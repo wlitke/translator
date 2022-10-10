5bd29069b2ee4dfeb67531aa3b08c11407abb489
Stephan Epp <Stephan_Epp@web.de>
2022-10-10 14:56:44 +0200

    Refactor names

--- translator/translator-console-app/Properties/AssemblyInfo.cs
+++ translator/translator-console-app/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("helloworld")]
+[assembly: AssemblyTitle("translator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("helloworld")]
+[assembly: AssemblyProduct("translator")]
 [assembly: AssemblyCopyright("Copyright ©  Microsoft 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
